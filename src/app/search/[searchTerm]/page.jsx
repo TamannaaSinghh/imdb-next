b869f7ac8e2e8f098b5199dac6cf66cd42cd8cd3
TamannaSinghh <tamannaasinghh1214.in>
2024-12-29 18:25:30 +0530

    updated search page

--- src/app/search/[searchTerm]/page.jsx
+++ src/app/search/[searchTerm]/page.jsx
@@ -2,7 +2,7 @@ import Results from "@@/components/Results";
 import React from "react";
 
 export default async function SearchPage({ params }) {
-  const searchTerm = params.searchTerm;
+  const searchTerm = await params.searchTerm;
   const res = await fetch(
     `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchTerm}&language=en-US&page=1&include_adult=false`
   );
